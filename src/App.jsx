8ad845fc20b0d8f37a84997f1d160b287bbdff6c
youzuos <1809452365@qq.com>
2026-05-21 19:51:00 +0800

    feat: assemble App component and fix CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import AuroraBackground from './components/AuroraBackground'
+import ParticleBackground from './components/ParticleBackground'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Contact from './components/Contact'
+import Experience from './components/Experience'
+import Education from './components/Education'
+import Projects from './components/Projects'
+import Leadership from './components/Leadership'
+import Footer from './components/Footer'
+
 function App() {
-  return <div className="min-h-screen flex items-center justify-center">Setting up Aurora...</div>
+  return (
+    <div className="min-h-screen">
+      <AuroraBackground />
+      <ParticleBackground />
+      <Navbar />
+      <main>
+        <Hero />
+        <Contact />
+        <Experience />
+        <Education />
+        <Projects />
+        <Leadership />
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
